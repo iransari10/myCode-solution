c949bddbb1e6abb6e7b9dcd7bf60ba0f4be416a6
iransari10 <ir.ansari10@gmail.com>
2020-11-07 23:01:46 +0530

    Update 1512. Number of Good Pairs.java

--- 1512. Number of Good Pairs.java
+++ 1512. Number of Good Pairs.java
@@ -1,9 +1,7 @@
 class Solution {
   
     public int numIdenticalPairs(int[] nums) {
-        int dp[] = new int[101];
         Map<Integer, Integer> map = new HashMap<>();
-        int ncr[] = new int[101];
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
